38a327d4ea74ab30b6582be12541844023c247e2
Alina <alina.chalaja@icloud.com>
2020-07-12 12:47:50 +0200

    My first project

---
+++ My-project/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String name = "Alina";
+        String surname= "Chala";
+        System.out.println(name+" "+surname);
+        System.out.println("The end");
+    }
+}
